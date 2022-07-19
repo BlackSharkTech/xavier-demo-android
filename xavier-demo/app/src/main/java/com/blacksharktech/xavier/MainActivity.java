df10893c70e0ba76e171fe5066af61a129d7ad7a
andreas.giannopoulos <andreas.giannopoulos@simoncomputing.com>
2022-07-19 17:33:33 -0400

    Added code that sets up the BaseCameraManager and the neccessary methods to simulate using an external camera

--- xavier-demo/app/src/main/java/com/blacksharktech/xavier/MainActivity.java
+++ xavier-demo/app/src/main/java/com/blacksharktech/xavier/MainActivity.java
@@ -1,31 +1,102 @@
 package com.blacksharktech.xavier;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.Size;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Surface;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.blacksharktech.xavierlib.CameraInterface;
+import com.blacksharktech.xavierlib.BaseCameraManager;
 import com.blacksharktech.xavierlib.Customization;
 import com.blacksharktech.xavierlib.XavierActivity;
 import com.blacksharktech.xavierlib.XavierError;
 import com.blacksharktech.xavierlib.XavierSDK;
+
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int XAVIER_RESULT = 1234;
 
     private Customization customization;
+    private BaseCameraManager baseCameraManager;
+    private CameraManager cameraManager;
+    private String cameraId;
+    private CameraDevice cameraDevice;
+    private Size previewSize;
+    private CameraCaptureSession cameraCaptureSession;
+    private Handler backgroundHandler;
+    private HandlerThread backgroundThread;
+    private CaptureRequest.Builder captureRequestBuilder;
+    private CaptureRequest captureRequest;
+    public static final String ERROR = "ERROR";
+
+    private static final String[] ALL_PERMISSIONS = new String[]{
+            Manifest.permission.CAMERA
+    };
+
+    private static final int PERMISSIONS_REQUEST_CAMERA = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Need to request permissions when using an external camera
+        ActivityCompat.requestPermissions(this, ALL_PERMISSIONS, PERMISSIONS_REQUEST_CAMERA);
+
         initCustomXavierUI();
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        baseCameraManager = new BaseCameraManager(new CameraInterface() {
+            @Override
+            public void openCamera() {
+                openBackgroundThread();
+                setUpCamera();
+                openCameraHelper();
+            }
+
+            @Override
+            public void closeCamera() {
+                closeCameraHelper();
+                closeBackgroundThread();
+            }
+
+            @Override
+            public void setSurface(SurfaceTexture surface) {
+                setSurfaceHelper(surface);
+            }
+
+            @Override
+            public Size getOutputSize() {
+                return previewSize;
+            }
+        });
 
         Button button = findViewById(R.id.startDemoButton);
         button.setOnClickListener(v -> {
@@ -33,9 +104,147 @@ public class MainActivity extends AppCompatActivity {
 
             XavierSDK.getInstance().setAppKey("$2a$12$NxGfKYhw8TuhXGTLGnvwD.C9RN799n3WgEHlZ2XqTEYwb65zuubLe");
             XavierSDK.getInstance().setCustomization(customization);
+            XavierSDK.getInstance().setBaseCameraManager(baseCameraManager);
 
             startActivityForResult(xavierActivity, XAVIER_RESULT);
         });
+    }
+
+    private void closeBackgroundThread() {
+        if (backgroundHandler != null) {
+            backgroundThread.quitSafely();
+            backgroundThread = null;
+            backgroundHandler = null;
+        }
+    }
+
+    private void openBackgroundThread() {
+        backgroundThread = new HandlerThread("camera_background_thread");
+        backgroundThread.start();
+        backgroundHandler = new Handler(backgroundThread.getLooper());
+    }
+
+    private CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice cameraDevice) {
+            MainActivity.this.cameraDevice = cameraDevice;
+            baseCameraManager.isOpened();
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice cameraDevice) {
+            cameraDevice.close();
+            baseCameraManager.isDisconnected();
+            MainActivity.this.cameraDevice = null;
+        }
+
+        @Override
+        public void onError(CameraDevice cameraDevice, int error) {
+            cameraDevice.close();
+            baseCameraManager.hasError();
+            MainActivity.this.cameraDevice = null;
+        }
+    };
+
+    private void setSurfaceHelper(SurfaceTexture previewSurface) {
+        try {
+            captureRequestBuilder = MainActivity.this.cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+
+            Surface surface = new Surface(previewSurface);
+            captureRequestBuilder.addTarget(surface);
+
+            MainActivity.this.cameraDevice.createCaptureSession(Collections.singletonList(surface),
+                    new CameraCaptureSession.StateCallback() {
+
+                        @Override
+                        public void onConfigured(CameraCaptureSession cameraCaptureSession) {
+                            if (MainActivity.this.cameraDevice == null) {
+                                Log.i("Xavier", "cameraDevice is null...");
+                                return;
+                            }
+
+                            try {
+                                captureRequest = captureRequestBuilder.build();
+                                MainActivity.this.cameraCaptureSession = cameraCaptureSession;
+                                MainActivity.this.cameraCaptureSession.setRepeatingRequest(captureRequest,
+                                        null, backgroundHandler);
+                            } catch (CameraAccessException e) {
+                                e.printStackTrace();
+                                baseCameraManager.hasError(e.getMessage());
+                            }
+                        }
+
+                        @Override
+                        public void onConfigureFailed(CameraCaptureSession cameraCaptureSession) {
+
+                        }
+                    }, backgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+            baseCameraManager.hasError(e.getMessage());
+        }
+    }
+
+    // Initializes the camera
+    private void setUpCamera() {
+        try {
+            for (String cameraId : cameraManager.getCameraIdList()) {
+                CameraCharacteristics cameraCharacteristics =
+                        cameraManager.getCameraCharacteristics(cameraId);
+                if (cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) ==
+                        CameraCharacteristics.LENS_FACING_BACK) {
+                    StreamConfigurationMap streamConfigurationMap = cameraCharacteristics.get(
+                            CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+
+                    Size[] sizes = streamConfigurationMap.getOutputSizes(SurfaceTexture.class);
+                    previewSize = chooseOptimalSize(sizes);
+
+                    this.cameraId = cameraId;
+
+                    Log.i("Xavier", "Setup camera...");
+                    Log.i("Xavier", cameraId);
+                }
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+            baseCameraManager.hasError(e.getMessage());
+        }
+    }
+
+    private void openCameraHelper() {
+        try {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_GRANTED) {
+                try{
+                    cameraManager.openCamera(cameraId, stateCallback, backgroundHandler);
+                    Log.i("Xavier", "Opened camera...");
+                }
+                catch (IllegalArgumentException e) {
+                    Intent intent = new Intent();
+                    intent.putExtra(ERROR, XavierError.CAMERA_DISABLED);
+                    setResult(RESULT_CANCELED, intent);
+                    finish();
+                }
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+            baseCameraManager.hasError(e.getMessage());
+        }
+    }
+
+    private void closeCameraHelper() {
+
+        if (cameraCaptureSession != null) {
+            cameraCaptureSession.close();
+            cameraCaptureSession = null;
+        }
+
+        if (cameraDevice != null) {
+            cameraDevice.close();
+            cameraDevice = null;
+            Log.i("Xavier", "Closed camera device...");
+        }
     }
 
     public void initCustomXavierUI() {
@@ -60,10 +269,15 @@ public class MainActivity extends AppCompatActivity {
 
             } else if(resultCode == RESULT_CANCELED){
                 if(data != null) {
+                    Log.d("Xavier", "RESULT_CANCELED Data: " + data.getSerializableExtra(XavierActivity.ERROR));
                     XavierError error = (XavierError) data.getSerializableExtra(XavierActivity.ERROR);
+                    String errorMessage = (String) data.getSerializableExtra(XavierActivity.ERROR_MESSAGE);
 
                     if (error != null) {
                         Toast.makeText(this, getErrorMessage(error), Toast.LENGTH_SHORT).show();
+                        if (errorMessage != null) {
+                            Log.i("Xavier", errorMessage);
+                        }
                     }
                 }
             }
@@ -99,8 +313,12 @@ public class MainActivity extends AppCompatActivity {
                 return getString(R.string.camErrorMaxInUse);
             case CAMERA_GENERIC:
                 return getString(R.string.camErrorDefault);
-            case CAMERA_ORIENTATION:
-                return getString(R.string.camErrorOrientation);
+            case EXTERNAL_CAMERA_DISCONNECTED:
+                return getString(R.string.extCamErrorDisconnected);
+            case EXTERNAL_CAMERA_GENERIC:
+                return getString(R.string.extCamErrorDefault);
+            case EXTERNAL_CAMERA_NOT_CONNECTED:
+                return getString(R.string.extCamErrorNotConnected);
             case LICENSE_INVALID:
                 return getString(R.string.invalidLicense);
             case PERMISSIONS:
@@ -109,6 +327,27 @@ public class MainActivity extends AppCompatActivity {
                 return getString(R.string.packageNotFound);
             default:
                 return getString(R.string.defaultError);
-      }
-  }
+        }
+    }
+
+    private Size chooseOptimalSize(Size[] outputSizes) {
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+
+        double preferredRatio = height / (double) width;
+        Size currentOptimalSize = outputSizes[0]; //set current to the first element
+        double currentOptimalRatio = currentOptimalSize.getWidth() / (double) currentOptimalSize.getHeight();
+        for (Size currentSize : outputSizes) {
+            double currentRatio = currentSize.getWidth() / (double) currentSize.getHeight();
+            //check if the ratio difference is smaller
+            if (Math.abs(preferredRatio - currentRatio) < Math.abs(preferredRatio - currentOptimalRatio)) {
+                currentOptimalSize = currentSize;
+                currentOptimalRatio = currentRatio;
+            }
+        }
+        return currentOptimalSize;
+    }
 }
